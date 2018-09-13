8b8b1b7f161e94c0b5b831f258abb82ee906c00e
Salocin Dot TEN (Nicolas Y) <salocin.ten@gmail.com>
2018-09-13 15:07:28 +0800

    Some more modifications.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi</h1>
-    <p>I am <a href="https://www.salocinten.info" target="_blank">Salocin.TEN</a></p>
+    <p>I am <a href="https://www.salocinten.info" target="_blank">Salocin.TEN</a>. A Web developer.</p>
     <p>I do not understand the need to make this thing using REACT JS.</p>
-    <p>It would be much simpler to use something else like Vue JS</p>
+    <p>It would be much simpler to use something else like Vue JS or to handcode the site.</p>
     <p>Anyway here you go, the thing to apply for the virtual hackathon.</p>
     <p>Thanks.</p>
   </div>
